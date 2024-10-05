7a091b5e4b8f05f76733e46b1a756bd7448071d6
Samuel Pruitt <pruitt.sam@gmail.com>
2024-10-04 20:45:49 -0500

    some changes to styling

--- svelteproject/myapp/src/routes/+layout.svelte
+++ svelteproject/myapp/src/routes/+layout.svelte
@@ -1,9 +1,26 @@
-<div class="navbar">
-    <a href="/">Home</a>
-    <a href="/SquadList">Team Squads</a>
-    <a href="/[userid]/savedsquads">Custom Squad Builder</a>
+<div class="navbar" class:collapsed={isOpen}>
+    <button class="toggle-button" on:click={() => isOpen = !isOpen}>
+        ☰
+    </button>
+
+    <a href="/" class="nav-item">
+        <i class="fas fa-home"></i>
+        <span class="nav-text">Home</span>
+    </a>
+    <a href="/SquadList" class="nav-item">
+        <i class="fas fa-users"></i>
+        <span class="nav-text">Team Squads</span>
+    </a>
+    <a href="/[userid]/savedsquads" class="nav-item">
+        <i class="fas fa-tools"></i>
+        <span class="nav-text">Custom Squad Builder</span>
+    </a>
 </div>
 
 <div>
     <slot />
 </div>
+
+<script>
+    let isOpen = false;
+</script>
